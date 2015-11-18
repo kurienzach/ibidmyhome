1ea7760ba7a924c7193924ebfe17e1a9888d1b2c
Kurien Zacharia <kurienzach@gmail.com>
2015-11-19 01:38:30 +0530

    Added coupons table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password', 60);
 
-            $table->string('mobile', 12);
+            $table->string('mobile', 15);
             $table->string('address', 200);
             $table->string('city', 50);
             $table->string('state', 50);
---
+++ database/migrations/2015_11_18_161134_create_coupons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('coupon_code', 50);
+            $table->boolean('used')->default(false);
+
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')
+                ->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('coupons');
+    }
+}
